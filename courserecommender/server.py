95ca1271207ce90a773455c1cd25c7eb22811a76
Michael Maltese <mike@mikemaltese.com>
2012-04-06 21:51:39 -0700

    Adds Flask-SQLAlchemy

--- courserecommender/server.py
+++ courserecommender/server.py
@@ -2,8 +2,11 @@ import os
 
 
 from flask import Flask
-app = Flask(__name__)
+from flaskext.sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:development.sqlite3")
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
