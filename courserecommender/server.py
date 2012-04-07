b5caba3558732406570fe27fec1f940d15f708dc
Michael Maltese <mike@mikemaltese.com>
2012-04-07 06:06:09 -0700

    Changes host to 0.0.0.0

--- courserecommender/server.py
+++ courserecommender/server.py
@@ -64,4 +64,4 @@ def logout():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
