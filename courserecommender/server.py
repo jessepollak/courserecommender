b77a51302e63b884c00a84341a0c8a3b39cee4fb
Joseph Long <josephoenix@gmail.com>
2012-04-06 22:03:28 -0700

    resolved merge conflict

--- courserecommender/server.py
+++ courserecommender/server.py
@@ -1,15 +1,14 @@
 import os
 
-
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template('course.html', name ="Elephant")
+def homepage():
+    return render_template('homepage.html')
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port, debug=True)
